61d00025cdcf11bcacbfc354fb0f56e5768aee99
Joern Huxhorn <jhuxhorn@googlemail.com>
2011-10-04 00:02:46 +0200

    First try to replicate LBCORE-224 issue separated from both Logback and Zookeeper builds.
    See http://jira.qos.ch/browse/LBCORE-224

---
+++ src/test/java/LBCORE224Test.java
@@ -0,0 +1,45 @@
+import org.apache.zookeeper.test.QuorumUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import java.io.File;
+
+// This test requires zookeeper 3.4.0 built from SVN trunk and setting
+// "build.test.dir" system property
+public class LBCORE224Test 
+{
+	private static final String BUILT_TEST_DIR_PROP_KEY = "build.test.dir";
+	private QuorumUtil qU;
+	
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+	
+	@Before
+	public void before() 
+	{
+		File tempDir = tempFolder.newFolder("foo");
+		System.setProperty(BUILT_TEST_DIR_PROP_KEY, tempDir.getAbsolutePath());
+		qU = new QuorumUtil(1);
+	}
+	
+	@After
+	public void after() throws Exception 
+	{
+		qU.tearDown();
+		System.clearProperty(BUILT_TEST_DIR_PROP_KEY);
+	}
+
+	@Test
+	public void shouldNotThrowIllegalMonitorStateException () throws Exception
+	{
+		for (int i = 0; i < 10; i++) 
+		{
+			qU.startQuorum();
+			qU.shutdownAll();
+			System.err.println("iteration "+i);
+		}
+	}
+}
+
